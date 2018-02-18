a71ab73b2352574eb11898f12dbf1a122d325afe
jussi.suojanen <jussi.suojanen@vincit.fi>
2018-02-18 18:40:12 +0200

    Add playground and framework for playground to project

---
+++ TestingCellsFramework/TestingCellsFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestingCellsFramework.h
+//  TestingCellsFramework
+//
+//  Created by Jussi Suojanen on 18/02/2018.
+//  Copyright © 2018 SwiftyJimmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestingCellsFramework.
+FOUNDATION_EXPORT double TestingCellsFrameworkVersionNumber;
+
+//! Project version string for TestingCellsFramework.
+FOUNDATION_EXPORT const unsigned char TestingCellsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingCellsFramework/PublicHeader.h>
+
+
